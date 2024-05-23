729d68a05b1b5eb70e1b2019ae1db9d64ca5a4f3
pancake3196 <104414194+pancake3196@users.noreply.github.com>
2024-05-23 16:41:09 +0900

    [level 0] Title: a와 b 출력하기, Time: 158.33 ms, Memory: 65.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        String sum1 = "a = "+a;
+        String sum2 = "b = "+b;
+            
+        System.out.println(sum1);
+        System.out.println(sum2);
+    }
+}
